1c3d498f46d665cdd6ab594e8010fc75c88188d9
julien <julien.genestoux@gmail.com>
2010-05-10 23:50:25 +0200

    now using a module approach for easier integration

--- lib/consistent_hashr.rb
+++ lib/consistent_hashr.rb
@@ -1,26 +1,18 @@
 require 'zlib'
+module ConsistentHashr
 
-class ConsistentHashr
-  
-  ##
-  # Creates a new Hasher. Servers should be a hash.
-  def initialize(_servers = {}, _number_of_replicas = 50)
-    @circle = {}
-    @number_of_replicas = _number_of_replicas
-    _servers.each do |name, server|
-      add_server(name, server)
-    end
-  end
+  @circle = {}
+  @number_of_replicas = 20
   
   ##
   # Computes a key
-  def hash_key(key)
+  def self.hash_key(key)
     Zlib.crc32("#{key}")
   end
 
   ##
   # Adds a server to the circle
-  def add_server(_name, _server)
+  def self.add_server(_name, _server)
     @number_of_replicas.times do |t|
       @circle[hash_key("#{_name}+#{t}")] = _server
     end
@@ -28,7 +20,7 @@ class ConsistentHashr
   
   ##
   # Returns the server for the provided key
-  def get(key)
+  def self.get(key)
     return nil if @circle.empty?
     return @circle.first.last if @circle.size == 1
     
--- spec/consistent_hashr_spec.rb
+++ spec/consistent_hashr_spec.rb
@@ -3,7 +3,9 @@ require File.dirname(__FILE__) + "/spec_helper"
 describe ConsistentHashr do
   before(:each) do
     @servers = {:s1 => "s1", :s2 => "s2", :s3 => "s3", :s4 => "s4", :s5 => "s5", :s6 => "s6"}
-    @hashr = ConsistentHashr.new(@servers)
+    @servers.each do |n, s|
+      ConsistentHashr.add_server(n,s)
+    end
   end
 
   it "should not change more than 75% of keys" do
@@ -11,14 +13,34 @@ describe ConsistentHashr do
     1000.times do |idx|
       keys << rand(100000).to_s
     end
-    before = keys.map() { |k|  @hashr.get(k)}
+    before = keys.map() { |k|  ConsistentHashr.get(k)}
     
-    @hashr.add_server(:s7, "s7")
+    ConsistentHashr.add_server(:s7, "s7")
     
-    after = keys.map() { |k|  @hashr.get(k)}
+    after = keys.map() { |k|  ConsistentHashr.get(k)}
     
     diff = before.zip(after).find_all {|a| a[0] == a[1] }.size
     diff.should > keys.size*0.7
+  end
+
+  it "should distribute keys evenly accross servers" do
+    keys = []
+    10000.times do |idx|
+      keys << rand(100000).to_s
+    end
+    servers = keys.map() { |k|  ConsistentHashr.get(k)}
+    
+    count_by_server = {}
+    servers.each do |s|
+      count_by_server[s] ||= 0
+      count_by_server[s] += 1
+    end
+    
+    min = count_by_server.invert.sort.first.first
+    max = count_by_server.invert.sort.last.first
+    
+    max.should < min * 2
+    
   end
 
 end
